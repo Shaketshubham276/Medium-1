fba20a05acc332dd41b735b08fb5ac7f030c1c27
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2024-09-04 04:13:44 +0530

    BlogSkeleton

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,20 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useBlog } from "../hooks";
 import { FullBlog } from "../components/FullBlog";
-import {BlogSkeleton} from "../components/BlogSkeleton"
+
 import { Appbar } from "../components/Appbar";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 export const Blog=()=>{
     const {id} =useParams();
     const {loading, blog} =useBlog({
         id: id || ""
     });
 
-    if (loading) {
+    if (loading || !blog) {
         return <div>
             <Appbar/>
             <div className="flex justify-center">
                 <div>
-                <BlogSkeleton />
+                <BlogSkeleton/>
+                
                 <BlogSkeleton />
                 <BlogSkeleton />
                 </div>
